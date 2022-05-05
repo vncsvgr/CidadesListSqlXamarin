36cf162613b1689bc253bd21b5230d6c4b06f294
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-05-04 22:19:53 -0300

    Adicionar arquivos de projeto.

---
+++ CidadesListSql/CidadesListSql/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CidadesListSql
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtualizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sbCidade_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void lstCidades_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private void btnFinalizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
