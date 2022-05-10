f1f4ea5cf9c1fed0f68f0118f762ea9faca9bc60
vncsvgr <vinicius.gabriel83@yahoo.com>
2022-05-09 22:33:18 -0300

    quase final

---
+++ CidadesListSql/CidadesListSql/Cidade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace CidadesListSql
+{
+    public class Cidade
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string uf { get; set; }
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataReader rConsulta;
+        string strConexao = "server=br536.hostgator.com.br;uid=dsetec86_aula;pwd=etecjau;database=dsetec86_bd_xamarin";
+
+        List<Cidade> cidades = new List<Cidade>();
+
+        public List<Cidade> Listar(string nome)
+        {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                comando = new MySqlCommand("select * from cidadew where nome like @nome order by nome", conexao);
+                comando.Parameters.AddWithValue("@nome", nome + "%");
+                rConsulta = comando.ExecuteReader();
+                while (rConsulta.Read())
+                {
+                    Cidade c = new Cidade()
+                    {
+                        id = int.Parse(rConsulta["ID"].ToString()),
+                        nome = rConsulta["NOME"].ToString(),
+                        uf = rConsulta["UF"].ToString()
+                    };
+                    cidades.Add(c);
+                }
+                conexao.Close();
+                return cidades;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public async void Incluir()
+        {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                comando = new MySqlCommand("insert into cidades (nome, uf) " +
+                                                       "values (@nome, @uf)", conexao);
+                comando.Parameters.AddWithValue("@nome", nome);
+                comando.Parameters.AddWithValue("@uf", uf);
+                comando.ExecuteNonQuery();
+                conexao.Close();
+            }
+            catch(Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public async void Atualizar()
+        {
+
+        }
+    }
+}
--- CidadesListSql/CidadesListSql/MainPage.xaml.cs
+++ CidadesListSql/CidadesListSql/MainPage.xaml.cs
@@ -10,14 +10,44 @@ namespace CidadesListSql
 {
     public partial class MainPage : ContentPage
     {
+        Cidade c = new Cidade();
+
         public MainPage()
         {
             InitializeComponent();
+            carregarDados();
+        }
+
+        void limpar()
+        {
+            txtID.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtUF.Text = string.Empty;
+            sbCidade.Text = string.Empty;
+        }
+
+        void carregarDados()
+        {
+            c = new Cidade();
+            lstCidades.ItemsSource = c.Listar(sbCidade.Text);
         }
 
         private void btnAdicionar_Clicked(object sender, EventArgs e)
         {
+            if(string.IsNullOrWhiteSpace(txtNome.Text) || string.IsNullOrWhiteSpace(txtUF.Text))
+            {
+                DisplayAlert("Atenção", "Favor completar as informações do cadastro", "OK");
+                return;
+            }
 
+            c = new Cidade
+            {
+                nome = txtNome.Text,
+                uf = txtUF.Text
+            };
+            c.Incluir();
+            limpar();
+            carregarDados();
         }
 
         private void btnAtualizar_Clicked(object sender, EventArgs e)
@@ -25,24 +55,32 @@ namespace CidadesListSql
 
         }
 
-        private void btnExcluir_Clicked(object sender, EventArgs e)
+        private async void btnExcluir_Clicked(object sender, EventArgs e)
         {
-
+            if (string.IsNullOrEmpty(txtID.Text))
+            {
+                await DisplayAlert("Atenção", "", "OK");
+                return;
+            }
         }
 
         private void sbCidade_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            carregarDados();
         }
 
         private void lstCidades_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            c = e.SelectedItem as Cidade;
 
+            txtID.Text = c.id.ToString();
+            txtNome.Text = c.nome;
+            txtUF.Text = c.uf;
         }
 
         private void btnFinalizar_Clicked(object sender, EventArgs e)
         {
-
+            Environment.Exit(0);
         }
     }
 }
